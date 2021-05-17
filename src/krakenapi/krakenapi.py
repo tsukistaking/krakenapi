af77954dfcb08476cdbcb15c2160338e97bd9f45
carumusan <879525+carumusan@users.noreply.github.com>
2021-05-17 14:40:39 +0100

    Simpligy api

--- src/krakenapi/krakenapi.py
+++ src/krakenapi/krakenapi.py
@@ -6,68 +6,63 @@ import base64
 import requests
 
 base_url = "https://api.kraken.com"
+API_KEY = None
+SECRET_KEY = None
 
 def nonce():
     return int(1000*time.time())
 
-def sign(secret_key, data, urlpath):
+def sign(data, urlpath):
+    if not SECRET_KEY:
+        raise Exception("SECRET_KEY not set")
+
     postdata = urllib.parse.urlencode(data)
 
     encoded = (str(data['nonce']) + postdata).encode()
     message = urlpath.encode() + hashlib.sha256(encoded).digest()
 
-    signature = hmac.new(base64.b64decode(secret_key),
+    signature = hmac.new(base64.b64decode(SECRET_KEY),
                             message, hashlib.sha512)
     sigdigest = base64.b64encode(signature.digest())
 
     return sigdigest.decode()
 
-def withdraw(api_key, secret_key, amount, asset, destination):
+def post_request(path, data):
+    if not API_KEY:
+        raise Exception("API_KEY not set") 
+    url  = base_url + path
+    data['nonce'] = nonce()
+    headers = {
+        'API-Key': API_KEY,
+        'API-Sign': sign(data, path)
+    }
+    response = requests.post(url, data=data, headers=headers)
+    response.raise_for_status()
+    print(response.text)
+    return response
+
+def withdraw(amount, asset, destination):
     withdraw_path = "/0/private/Withdraw"
-    full_withdraw_url = base_url + withdraw_path
     data = {
-        'nonce': nonce(),
         'asset': asset,
         'key': destination,
         'amount': amount
     }
-    headers = {
-        'API-Key': api_key,
-        'API-Sign': sign(secret_key, data, withdraw_path)
-    }
-    response = requests.post(full_withdraw_url, data=data, headers=headers)
-    print(response.text)
+    post_request(withdraw_path, data)
 
-def balance(api_key, secret_key, asset):
+def balance(asset):
     balance_path = "/0/private/Balance"
-    balance_url = base_url + balance_path
-    balance_data = {
-        'nonce': nonce(),
-    }
-    balance_headers = {
-        'API-Key': api_key,
-        'API-Sign': sign(secret_key, balance_data, balance_path)
-    }
-    response = requests.post(balance_url, data=balance_data, headers=balance_headers)
-    response.raise_for_status()
+    response = post_request(balance_path, {})
     balance = float(response.json()['result'].get(asset, 0))
     return balance
 
-def market_trade(api_key, secret_key, pair, side, amount):
+def market_trade(pair, side, amount):
     trade_path = "/0/private/AddOrder"
-    trade_url = base_url + trade_path
     trade_data = {
-        'nonce': nonce(),
         'pair': pair,
         'type': side,
         'ordertype': "market",
         'volume': amount
     }
-    trade_headers = {
-        'API-Key': api_key,
-        'API-Sign': sign(secret_key, trade_data, trade_path)
-    }
-    response = requests.post(trade_url, data=trade_data, headers=trade_headers)
-    response.raise_for_status()
-    print(response.text)
+    post_request(trade_path, trade_data)
 
