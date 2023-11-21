ec06a0ca84e1bbca1b11e735e1e1c1874f154968
carumasan <879525+carumusan@users.noreply.github.com>
2023-11-21 14:23:36 +0000

    Raise exceptioon if error or no result in response

--- src/krakenapi/krakenapi.py
+++ src/krakenapi/krakenapi.py
@@ -38,8 +38,13 @@ def post_request(path, data):
     }
     response = requests.post(url, data=data, headers=headers)
     response.raise_for_status()
-    print(response.text)
-    return response
+    response_json = response.json()
+    if error := response_json.get('error'):
+        raise Exception(error)
+    if not (result := response_json.get('result')):
+        raise Exception(f"'result' key not found in json: {response}")
+    print(result)
+    return result
 
 def withdraw(amount, asset, destination):
     withdraw_path = "/0/private/Withdraw"
@@ -53,7 +58,7 @@ def withdraw(amount, asset, destination):
 def balance(asset):
     balance_path = "/0/private/Balance"
     response = post_request(balance_path, {})
-    balance = float(response.json()['result'].get(asset, 0))
+    balance = float(response.get(asset, 0))
     return balance
 
 def market_trade(pair, side, amount):
