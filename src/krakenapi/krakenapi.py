898ca2ae5d60c185ff12dca115f8eaab9fba5cf4
carumasan <879525+carumusan@users.noreply.github.com>
2024-11-08 02:26:53 +0000

    Make krakenapi async

--- src/krakenapi/krakenapi.py
+++ src/krakenapi/krakenapi.py
@@ -1,3 +1,4 @@
+import asyncio
 import base64
 import hashlib
 import hmac
@@ -5,7 +6,7 @@ import math
 import time
 import urllib
 
-import requests
+import aiohttp
 
 base_url = "https://api.kraken.com"
 API_KEY = None
@@ -29,7 +30,7 @@ def sign(data, urlpath):
 
     return sigdigest.decode()
 
-def post_request(path, data):
+async def post_request(path, data):
     if not API_KEY:
         raise Exception("API_KEY not set") 
     url  = base_url + path
@@ -38,45 +39,46 @@ def post_request(path, data):
         'API-Key': API_KEY,
         'API-Sign': sign(data, path)
     }
-    response = requests.post(url, data=data, headers=headers)
-    response.raise_for_status()
-    response_json = response.json()
-    if error := response_json.get('error'):
-        raise Exception(error)
-    if not (result := response_json.get('result')):
-        raise Exception(f"'result' key not found in json: {response}")
-    return result
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=data, headers=headers) as response:
+            response.raise_for_status()
+            response_json = await response.json()
+            if error := response_json.get('error'):
+                raise Exception(error)
+            if not (result := response_json.get('result')):
+                raise Exception(f"'result' key not found in json: {response}")
+            return result
 
-def withdraw(amount, asset, destination):
+async def withdraw(amount, asset, destination):
     withdraw_path = "/0/private/Withdraw"
     data = {
         'asset': asset,
         'key': destination,
         'amount': amount
     }
-    post_request(withdraw_path, data)
+    await post_request(withdraw_path, data)
 
-def all_balances():
+async def all_balances():
     balance_path = "/0/private/Balance"
-    response = post_request(balance_path, {})
+    response = await post_request(balance_path, {})
     return response
 
-def balance(asset):
-    all_balances_response = all_balances()
+async def balance(asset):
+    all_balances_response = await all_balances()
     balance = float(all_balances_response[asset])
     return balance
 
-def all_balances_extended():
+async def all_balances_extended():
     balance_extended_path = "/0/private/BalanceEx"
-    response = post_request(balance_extended_path, {})
+    response = await post_request(balance_extended_path, {})
     return response
 
-def balance_extended(asset):
-    all_balances_extended_response = all_balances_extended()
+async def balance_extended(asset):
+    all_balances_extended_response = await all_balances_extended()
     balance = float(all_balances_extended_response[asset])
     return balance
 
-def market_trade(pair, side, amount):
+async def market_trade(pair, side, amount):
     trade_path = "/0/private/AddOrder"
     trade_data = {
         'pair': pair,
@@ -84,34 +86,34 @@ def market_trade(pair, side, amount):
         'ordertype': "market",
         'volume': amount
     }
-    post_request(trade_path, trade_data)
+    await post_request(trade_path, trade_data)
 
-def list_earn_strategies():
+async def list_earn_strategies():
     earn_path = "/0/private/Earn/Strategies"
-    response = post_request(earn_path, {})
+    response = await post_request(earn_path, {})
     return response['items']
 
-def allocate_earn_funds(strategy_id, amount):
+async def allocate_earn_funds(strategy_id, amount):
     allocate_path = "/0/private/Earn/Allocate"
     allocate_data = {
         'strategy_id': strategy_id,
         'amount': amount
     }
-    post_request(allocate_path, allocate_data)
+    await post_request(allocate_path, allocate_data)
 
-def allocate_status(strategy_id):
+async def allocate_status(strategy_id):
     status_path = "/0/private/Earn/AllocateStatus"
     status_data = {
         'strategy_id': strategy_id
     }
-    return post_request(status_path, status_data)
+    return await post_request(status_path, status_data)
 
-def list_earn_allocations():
+async def list_earn_allocations():
     allocations_path = "/0/private/Earn/Allocations"
-    response = post_request(allocations_path, {})
+    response = await post_request(allocations_path, {})
     return response['items']
 
-def trade_history(from_datetime = None, to_datetime = None):
+async def trade_history(from_datetime = None, to_datetime = None):
     trade_history_path = "/0/private/TradesHistory"
     trade_history_data = {
         "ofs": 0
@@ -124,7 +126,7 @@ def trade_history(from_datetime = None, to_datetime = None):
     trade_count = 50
     offset_page = 0
     while offset_page < math.ceil(trade_count / 50):
-        response = post_request(trade_history_path, trade_history_data)
+        response = await post_request(trade_history_path, trade_history_data)
         trades |= response['trades']
         trade_count = response['count']
         offset_page += 1
@@ -132,6 +134,6 @@ def trade_history(from_datetime = None, to_datetime = None):
         # Decaying rate limit. Decays at 0.33 requests per second.
         # Limit is 15/20 and history requests are 2 requests.
         if offset_page > 6:
-            time.sleep(7)
+            await asyncio.sleep(7)
     
     return trades
