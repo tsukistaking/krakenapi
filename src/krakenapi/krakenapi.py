d0a57b909b8194601840cee5fcfbebb8cc615051
carumasan <879525+carumusan@users.noreply.github.com>
2024-11-15 09:58:11 +0000

    Use nanoseconds for nonce and pass in session to all calls

--- src/krakenapi/krakenapi.py
+++ src/krakenapi/krakenapi.py
@@ -6,96 +6,80 @@ import math
 import time
 import urllib
 
-import aiohttp
-
 base_url = "https://api.kraken.com"
 API_KEY = None
 SECRET_KEY = None
 
 def nonce():
-    return int(1000000*time.time())
+    return int(1000000000 * time.time())
 
 def sign(data, urlpath):
     if not SECRET_KEY:
         raise Exception("SECRET_KEY not set")
 
     postdata = urllib.parse.urlencode(data)
-
     encoded = (str(data['nonce']) + postdata).encode()
     message = urlpath.encode() + hashlib.sha256(encoded).digest()
 
     signature = hmac.new(base64.b64decode(SECRET_KEY),
-                            message, hashlib.sha512)
+                         message, hashlib.sha512)
     sigdigest = base64.b64encode(signature.digest())
 
     return sigdigest.decode()
 
 nonce_lock = asyncio.Lock()
 
-session = None
-
-async def get_session():
-    global session
-    if session is None:
-        session = aiohttp.ClientSession(raise_for_status=True)
-    return session
-
-async def post_request(path, data):
+async def post_request(session, path, data):
     if not API_KEY:
-        raise Exception("API_KEY not set") 
-    url  = base_url + path
-    await nonce_lock.acquire()
-    data['nonce'] = nonce()
+        raise Exception("API_KEY not set")
+    url = base_url + path
+
+    async with nonce_lock:
+        data['nonce'] = nonce()
+    
     headers = {
         'API-Key': API_KEY,
         'API-Sign': sign(data, path)
     }
 
-    async def on_headers_sent(session, trace_config_ctx, params):
-        if nonce_lock.locked():
-            nonce_lock.release()
+    async with session.post(url, data=data, headers=headers) as response:
+        response_json = await response.json()
+        if error := response_json.get('error'):
+            raise Exception(f"Kraken API error: {error}")
+        if not (result := response_json.get('result')):
+            raise Exception(f"'result' key not found in response: {response_json}")
+        return result
 
-    trace_config = aiohttp.TraceConfig()
-    trace_config.on_request_headers_sent.append(on_headers_sent)
-    async with aiohttp.ClientSession(raise_for_status=True, trace_configs=[trace_config]) as session:
-        async with session.post(url, data=data, headers=headers) as response:
-            response_json = await response.json()
-            if error := response_json.get('error'):
-                raise Exception(error)
-            if not (result := response_json.get('result')):
-                raise Exception(f"'result' key not found in json: {response}")
-            return result
-
-async def withdraw(amount, asset, destination):
+async def withdraw(session, amount, asset, destination):
     withdraw_path = "/0/private/Withdraw"
     data = {
         'asset': asset,
         'key': destination,
         'amount': amount
     }
-    await post_request(withdraw_path, data)
+    await post_request(session, withdraw_path, data)
 
-async def all_balances():
+async def all_balances(session):
     balance_path = "/0/private/Balance"
-    response = await post_request(balance_path, {})
+    response = await post_request(session, balance_path, {})
     return response
 
-async def balance(asset):
-    all_balances_response = await all_balances()
-    balance = float(all_balances_response[asset])
+async def balance(session, asset):
+    all_balances_response = await all_balances(session)
+    balance = float(all_balances_response.get(asset, 0))
     return balance
 
-async def all_balances_extended():
+async def all_balances_extended(session):
     balance_extended_path = "/0/private/BalanceEx"
-    response = await post_request(balance_extended_path, {})
+    response = await post_request(session, balance_extended_path, {})
     return response
 
-async def balance_extended(asset):
-    all_balances_extended_response = await all_balances_extended()
-    balance = float(all_balances_extended_response[asset])
+async def balance_extended(session, asset):
+    all_balances_extended_response = await all_balances_extended(session)
+    balance = float(all_balances_extended_response.get(asset, 0))
     return balance
 
-async def market_trade(pair, side, amount):
+async def market_trade(session, pair, side, amount):
     trade_path = "/0/private/AddOrder"
     trade_data = {
         'pair': pair,
@@ -103,34 +87,34 @@ async def market_trade(pair, side, amount):
         'ordertype': "market",
         'volume': amount
     }
-    await post_request(trade_path, trade_data)
+    await post_request(session, trade_path, trade_data)
 
-async def list_earn_strategies():
+async def list_earn_strategies(session):
     earn_path = "/0/private/Earn/Strategies"
-    response = await post_request(earn_path, {})
+    response = await post_request(session, earn_path, {})
     return response['items']
 
-async def allocate_earn_funds(strategy_id, amount):
+async def allocate_earn_funds(session, strategy_id, amount):
     allocate_path = "/0/private/Earn/Allocate"
     allocate_data = {
         'strategy_id': strategy_id,
         'amount': amount
     }
-    await post_request(allocate_path, allocate_data)
+    await post_request(session, allocate_path, allocate_data)
 
-async def allocate_status(strategy_id):
+async def allocate_status(session, strategy_id):
     status_path = "/0/private/Earn/AllocateStatus"
     status_data = {
         'strategy_id': strategy_id
     }
-    return await post_request(status_path, status_data)
+    return await post_request(session, status_path, status_data)
 
-async def list_earn_allocations():
+async def list_earn_allocations(session):
     allocations_path = "/0/private/Earn/Allocations"
-    response = await post_request(allocations_path, {})
+    response = await post_request(session, allocations_path, {})
     return response['items']
 
-async def trade_history(from_datetime = None, to_datetime = None):
+async def trade_history(session, from_datetime=None, to_datetime=None):
     trade_history_path = "/0/private/TradesHistory"
     trade_history_data = {
         "ofs": 0
@@ -143,8 +127,8 @@ async def trade_history(from_datetime = None, to_datetime = None):
     trade_count = 50
     offset_page = 0
     while offset_page < math.ceil(trade_count / 50):
-        response = await post_request(trade_history_path, trade_history_data)
-        trades |= response['trades']
+        response = await post_request(session, trade_history_path, trade_history_data)
+        trades.update(response['trades'])
         trade_count = response['count']
         offset_page += 1
         trade_history_data['ofs'] = offset_page * 50
@@ -152,5 +136,5 @@ async def trade_history(from_datetime = None, to_datetime = None):
         # Limit is 15/20 and history requests are 2 requests.
         if offset_page > 6:
             await asyncio.sleep(7)
-    
+
     return trades
